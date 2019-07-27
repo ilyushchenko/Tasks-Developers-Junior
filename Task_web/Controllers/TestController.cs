b9a18799bf6ced6babfd3408127c6bd8a0299198
Pavel Ilyushchenko <pavel.ks1996@gmail.com>
2019-07-27 13:55:14 +0300

    Web task completed

--- Task_web/Controllers/TestController.cs
+++ Task_web/Controllers/TestController.cs
@@ -17,7 +17,107 @@ namespace Task_web.Controllers
         {
             _testModels = testModels;
         }
-        
+
+        /// <summary>
+        /// Возвращает коллекцию моделей
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<TestModel[]> GetAll()
+        {
+            return _testModels.ToArray();
+        }
+
+        /// <summary>
+        /// Возвращает конкретную модель по ID
+        /// </summary>
+        /// <param name="id">Идентификатор модели</param>
+        /// <returns></returns>
+        [HttpGet("{id:Guid}")]
+        public ActionResult<TestModel> GetById(Guid id)
+        {
+            var model = _testModels.FirstOrDefault(m => m.Id == id);
+            return model == null ? NotFound() : (ActionResult<TestModel>)model;
+        }
+
+        /// <summary>
+        /// Создает <see cref="TestModel"/>
+        /// </summary>
+        /// <param name="model">Данные модели</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult<TestModel> Create([FromBody]TestModel model)
+        {
+            ValidateModel(model);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            model.Id = Guid.NewGuid();
+            _testModels.Add(model);
+            return CreatedAtAction(nameof(GetById), new { id = model.Id }, model);
+        }
+
+        /// <summary>
+        /// Обновляет данные <see cref="TestModel"/>
+        /// </summary>
+        /// <param name="id">Идентификатор модели</param>
+        /// <param name="model">Данные модели</param>
+        /// <returns></returns>
+        [HttpPut("{id:Guid}")]
+        public ActionResult<TestModel> Update(Guid id, [FromBody]TestModel model)
+        {
+            if (model.Id != id)
+            {
+                ModelState.AddModelError("invalidId", "Указан неправильный идентификатор");
+                BadRequest(ModelState);
+            }
+
+            var dbModel = _testModels.FirstOrDefault(m => m.Id == id);
+            if (dbModel == null)
+                return NotFound();
+
+            ValidateModel(model);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var index = _testModels.IndexOf(dbModel);
+            _testModels[index] = model;
+
+            return new NoContentResult();
+        }
+
+        /// <summary>
+        /// Удаляет модель по Id
+        /// </summary>
+        /// <param name="id">Идентификатор модели</param>
+        /// <returns></returns>
+        [HttpDelete("{id:Guid}")]
+        public IActionResult Delete(Guid id)
+        {
+            var model = _testModels.FirstOrDefault(m => m.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            _testModels.Remove(model);
+            return new NoContentResult();
+        }
+
+        #region Helpers
+
+        private void ValidateModel(TestModel model)
+        {
+            if (string.IsNullOrEmpty(model.Name))
+            {
+                ModelState.AddModelError("emptyName", "Укажите Name");
+            }
+        }
+
+        #endregion
+
 
         /*
          *
